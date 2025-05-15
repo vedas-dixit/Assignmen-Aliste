baada489e4cbfc5aa2eb7a92f11177675756692b
vedas-dixit <vedasdixit1006@gmail.com>
2025-05-15 18:23:45 +0530

    basic-app setup

---
+++ shopping-app/app/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color:'white'
+  },
+}); 
